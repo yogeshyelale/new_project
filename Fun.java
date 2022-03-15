1c4b75f65bb00ce64ebdeb22cba39acc9bfb564c
yogeshyelale <101618091+yogeshyelale@users.noreply.github.com>
2022-03-15 12:07:39 +0530

    Add files via upload

---
+++ Fun.java
@@ -0,0 +1,9 @@
+class Fun{
+	  static int add(int n1, int n2)
+	  { int result =n1+n2;
+	  return result ;}
+	  public static void main (String ar[])
+	  {
+		  System.out.println(add(5,7));
+	  }
+}
